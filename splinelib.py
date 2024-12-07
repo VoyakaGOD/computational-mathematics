1c0ea30fab7ddf8f7ee91bf95ea7da5b99a8ee9c
VoyakaGOD <ab3000645@gmail.com>
2024-12-07 17:33:34 +0300

    lab 5: inegration of spline with oscillations

--- splinelib.py
+++ splinelib.py
@@ -1,7 +1,24 @@
+from math import sin, cos
+
 def calculate_spline(x, next_x, prev_x, next_m, prev_m, alpha, beta, h):
     next_dx = next_x - x
     prev_dx = x - prev_x
     return (prev_m * next_dx**3 + next_m * prev_dx**3 + alpha * next_dx + beta * prev_dx) / h
+
+def integrate_spline_with_oscillations(next_x, prev_x, next_m, prev_m, alpha, beta, h, k):
+    c3 = next_m - prev_m
+    c2 = 3 * (prev_m * next_x - next_m * prev_x)
+    c1 = beta - alpha - 3 * (prev_m * next_x * next_x - next_m * prev_x * prev_x)
+    c0 = alpha * next_x - beta * prev_x + prev_m * next_x * next_x * next_x - next_m * prev_x * prev_x * prev_x
+    I0 = (cos(k * prev_x) - cos(k * next_x)) / k
+    I1 = (sin(k*next_x) - k * next_x * cos(k * next_x) - sin(k*prev_x) + k * prev_x * cos(k * prev_x)) / k / k
+    I2 = (2 * k * next_x) * sin(k * next_x) + (2 - k*k*next_x*next_x) * cos(k * next_x)
+    I2 -= (2 * k * prev_x) * sin(k * prev_x) + (2 - k*k*prev_x*prev_x) * cos(k * prev_x)
+    I2 /= k*k*k
+    I3 = (3*k*k*next_x*next_x - 6) * sin(k * next_x) + (6 - k*k*next_x*next_x) * k * next_x * cos(k * next_x)
+    I3 -= (3*k*k*prev_x*prev_x - 6) * sin(k * prev_x) + (6 - k*k*prev_x*prev_x) * k * prev_x * cos(k * prev_x)
+    I3 /= k*k*k*k
+    return (c0*I0 + c1*I1 + c2*I2 + c3*I3) / h
 
 class NaturalSpline():
     def __init__(self, xs : list[float], ys : list[float], left_second_derivative : float = 0, right_second_derivative : float = 0):
@@ -36,3 +53,12 @@ class NaturalSpline():
         if x > self.xs[-1]:
             return calculate_spline(x, self.xs[-1], self.xs[-2], self.m[-1], self.m[-2], self.alpha[-1], self.beta[-1], self.h[-1])
         raise Exception("bad spline")
+
+    #strictly integrate S(x)*sin(kx) from x_0 to x_n
+    def integrate_with_oscillations(self, k : float) -> float:
+        I = 0
+        for i in range(self.n):
+            I += integrate_spline_with_oscillations(self.xs[i+1], self.xs[i], self.m[i+1], self.m[i], 
+                                                    self.alpha[i], self.beta[i], self.h[i], k)
+        return I
+    
