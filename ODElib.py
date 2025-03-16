b0aa93bf7b9873f0f2a5fbfd2b61c5896ecd4453
VoyakaGOD <ab3000645@gmail.com>
2025-03-16 22:45:53 +0300

    non-stiff-ODE-systems: Adams methods

--- ODElib.py
+++ ODElib.py
@@ -5,39 +5,73 @@ VecFunc = Callable[[float, Vector], Vector]
 
 # t_n = t_0 + nh, n in [0, N]
 class RungeKuttaMethods:
+    @staticmethod
     def explicit_1_order(f : VecFunc, u0 : Vector, h : float, N : int, t0 : float = 0):
         u = [u0]
         for n in range(N):
             t = t0 + n * h
             u += [u[n] + h * f(t, u[n])]
         return u
-    
+
+    @staticmethod
     def explicit_2_order(f : VecFunc, u0 : Vector, h : float, N : int, t0 : float = 0):
         u = [u0]
         for n in range(N):
             t = t0 + n * h
             k1 = f(t, u[n])
-            k2 = f(t, u[n] + h / 2 * k1)
+            k2 = f(t + h / 2, u[n] + h / 2 * k1)
             u += [u[n] + h * k2]
         return u
-    
+
+    @staticmethod
     def explicit_3_order(f : VecFunc, u0 : Vector, h : float, N : int, t0 : float = 0):
         u = [u0]
         for n in range(N):
             t = t0 + n * h
             k1 = f(t, u[n])
-            k2 = f(t, u[n] + h * k1/3)
-            k3 = f(t, u[n] + 2/3 * h * k2)
+            k2 = f(t + h/3, u[n] + h * k1/3)
+            k3 = f(t + 2/3 * h, u[n] + 2/3 * h * k2)
             u += [u[n] + h * 0.25 * k1 + h * 0.75 * k3]
         return u
 
+    @staticmethod
     def explicit_4_order(f : VecFunc, u0 : Vector, h : float, N : int, t0 : float = 0):
         u = [u0]
         for n in range(N):
             t = t0 + n * h
             k1 = f(t, u[n])
-            k2 = f(t, u[n] + h * k1/2)
-            k3 = f(t, u[n] + h * k2/2)
-            k4 = f(t, u[n] + h * k3)
+            k2 = f(t + h/2, u[n] + h * k1/2)
+            k3 = f(t + h/2, u[n] + h * k2/2)
+            k4 = f(t + h, u[n] + h * k3)
             u += [u[n] + h/6 * (k1 + 2*k2 + 2*k3 + k4)]
+        return u
+
+# u0 = [u_0, ..., u_p], p - order of method
+class AdamsMethods:
+    @staticmethod
+    def explicit_1_order(f : VecFunc, u0 : list[Vector], h : float, N : int, t0 : float = 0):
+        return RungeKuttaMethods.explicit_1_order(f, u0[0], h, N, t0)
+    
+    @staticmethod
+    def explicit_2_order(f : VecFunc, u0 : list[Vector], h : float, N : int, t0 : float = 0):
+        u = u0.copy()
+        for n in range(1, N):
+            t = t0 + n * h
+            u += [u[n] + h/2 * (3*f(t, u[n]) - f(t-h, u[n-1]))]
+        return u
+
+    @staticmethod
+    def explicit_3_order(f : VecFunc, u0 : list[Vector], h : float, N : int, t0 : float = 0):
+        u = u0.copy()
+        for n in range(2, N):
+            t = t0 + n * h
+            u += [u[n] + h/12 * (23*f(t, u[n]) - 16*f(t-h, u[n-1]) + 5*f(t-2*h, u[n-2]))]
+        return u
+
+    @staticmethod
+    def explicit_4_order(f : VecFunc, u0 : list[Vector], h : float, N : int, t0 : float = 0):
+        u = u0.copy()
+        for n in range(3, N):
+            t = t0 + n * h
+            u += [u[n] + h/24 * (55*f(t, u[n]) - 59*f(t-h, u[n-1]) + 37*f(t-2*h, u[n-2]) - 9*f(t-3*h, u[n-3]))]
         return u
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ![Jupyter Notebook](https://img.shields.io/badge/Jupyter_Notebook-F37726)
-![repo size](https://img.shields.io/badge/repo_size-391_kb-blue)
+![repo size](https://img.shields.io/badge/repo_size-392_kb-blue)
 
 Repository with laboratory works in computational mathematics.
 
