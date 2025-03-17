5378f8713c279d4df87e69ca7d2d977d7ff096f9
VoyakaGOD <ab3000645@gmail.com>
2025-03-17 03:20:17 +0300

    non-stiff-ODE-systems: BDF

--- ODElib.py
+++ ODElib.py
@@ -75,3 +75,30 @@ class AdamsMethods:
             t = t0 + n * h
             u += [u[n] + h/24 * (55*f(t, u[n]) - 59*f(t-h, u[n-1]) + 37*f(t-2*h, u[n-2]) - 9*f(t-3*h, u[n-3]))]
         return u
+
+# Backward Differentiation Formulas
+# u0 = [u_0, ..., u_p], p - order of method
+class BDF:
+    def explicit_1_order(f : VecFunc, u0 : list[Vector], h : float, N : int, t0 : float = 0):
+        return AdamsMethods.explicit_1_order(f, u0, h, N, t0)
+    
+    def explicit_2_order(f : VecFunc, u0 : list[Vector], h : float, N : int, t0 : float = 0):
+        u = u0.copy()
+        for n in range(1, N):
+            t = t0 + n * h
+            u += [u[n-1] + 2*h * f(t, u[n])]
+        return u
+
+    def explicit_3_order(f : VecFunc, u0 : list[Vector], h : float, N : int, t0 : float = 0):
+        u = u0.copy()
+        for n in range(2, N):
+            t = t0 + n * h
+            u += [-3/2*u[n] + 3*u[n-1] - 1/2*u[n-2] + 3*h * f(t, u[n])]
+        return u
+
+    def explicit_4_order(f : VecFunc, u0 : list[Vector], h : float, N : int, t0 : float = 0):
+        u = u0.copy()
+        for n in range(3, N):
+            t = t0 + n * h
+            u += [145/47*u[n] - 114/47*u[n-1] + 5/47*u[n-2] + 1/47*u[n-3] + 13*h * f(t, u[n])]
+        return u
